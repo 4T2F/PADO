62d3425655897b15e96cde0bd7eda33b1cf834b0
kmh5038 <kmh5038@naver.com>
2024-01-16 13:48:31 +0900

    💄 Design: ReportView UI 작업
    💄 Design: ReportView UI 작업

---
+++ PADO/PADO/iOS/MainView/Report/ReportSelectCellView.swift
@@ -0,0 +1,39 @@
+//
+//  ReportSelectCellView.swift
+//  PADO
+//
+//  Created by 김명현 on 1/16/24.
+//
+
+import SwiftUI
+
+struct ReportSelectCellView: View {
+    var text: String
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: UIScreen.main.bounds.width, height: 45)
+                .foregroundStyle(Color(.systemGray5))
+            HStack {
+                
+                Text(text)
+                    .foregroundStyle(.white)
+                    .font(.system(size: 16))
+                    .fontWeight(.medium)
+                
+                Spacer()
+                
+                Image(systemName: "arrow.right")
+                    .foregroundStyle(.white)
+                    .font(.system(size: 16))
+                    .fontWeight(.medium)
+            }
+            .padding(.horizontal, UIScreen.main.bounds.width * 0.03)
+            .frame(height: 30)
+        }
+    }
+}
+
+#Preview {
+    ReportSelectCellView(text: "신고하기")
+}
