b06843b5314abb50bb5bc8f6ef20d914974904ed
DongHo Choe <hamfan524@naver.com>
2024-01-16 20:18:39 +0900

    💄 Design: MainView UI 구현 완료
    댓글, 신고화면 UI구현 완료

--- PADO/PADO/PADOApp.swift
+++ PADO/PADO/PADOApp.swift
@@ -27,7 +27,7 @@ struct PADOApp: App {
     
     var body: some Scene {
         WindowGroup {
-            StartView()
+            MainView()
         }
         .environmentObject(AuthenticationViewModel())
     }
