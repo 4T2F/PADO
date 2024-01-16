db6a070f0e1b54395ae07180737338745d2dd2a3
Hsungjin <hsungjin142@gmail.com>
2024-01-16 10:29:06 +0900

    🐞 Fix: Build Phase 경고 해결

---
+++ PADO/PADO/iOS/MyFeed/View/FollowingView.swift
@@ -0,0 +1,18 @@
+//
+//  FollowingView.swift
+//  PADO
+//
+//  Created by 황성진 on 1/16/24.
+//
+
+import SwiftUI
+
+struct FollowingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FollowingView()
+}
