eedf30d505a1fdd6435b7f02c4800ed5669bda1b
DongHo Choe <hamfan524@naver.com>
2024-01-16 16:27:18 +0900

    💄 Design: 댓글 모달 시트 View 구현
    댓글 셀, 텍스트 필드 구현
    
    추가 구현해야 할 사항
    FaceMojiView, FaceMojiCell

---
+++ PADO/PADO/iOS/MainView/Comment/FaceMojiCell.swift
@@ -0,0 +1,18 @@
+//
+//  FaceMojiCell.swift
+//  PADO
+//
+//  Created by 최동호 on 1/16/24.
+//
+
+import SwiftUI
+
+struct FaceMojiCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FaceMojiCell()
+}
---
+++ PADO/PADO/iOS/MainView/Comment/FaceMojiView.swift
@@ -0,0 +1,18 @@
+//
+//  FaceMoji.swift
+//  PADO
+//
+//  Created by 최동호 on 1/16/24.
+//
+
+import SwiftUI
+
+struct FaceMojiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FaceMojiView()
+}
