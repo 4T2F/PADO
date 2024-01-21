bba7ba13b3660402cbc088d857b7bd2fc4bd492c
DongHo Choe <hamfan524@naver.com>
2024-01-21 16:35:30 +0900

    🌊 Feat: 로그인 기능 구현
    추가 구현해야할 사항
    - 가입되지 않은 번호 일 시 회원가입하게 바로 이동
    - 전화번호 입력 시 바로 가입 여부 판단하게 수정 해야함

--- PADO/PADO/iOS/Login/LoginView.swift
+++ PADO/PADO/iOS/Login/LoginView.swift
@@ -13,11 +13,65 @@ enum LoginStep {
 }
 
 struct LoginView: View {
+    @ObservedObject var viewModel: AuthenticationViewModel
+    @Environment(\.dismiss) var dismiss
+    @State var currentStep: LoginStep = .phoneNumber
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            Color.mainBackground.ignoresSafeArea()
+            VStack {
+                ZStack {
+                    Text("PADO")
+                        .font(.system(size: 22))
+                        .fontWeight(.bold)
+                    
+                    HStack {
+                        Button {
+                            handleBackButton()
+                        } label: {
+                            Image("dismissArrow")
+                                .foregroundStyle(.white)
+                                .font(.system(size: 22))
+                        }
+                        
+                        Spacer()
+                    }
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+            }
+            
+            switch currentStep {
+            case .phoneNumber:
+                LoginPhoneNumberView(currentStep: $currentStep,
+                                viewModel: viewModel)
+            case .code:
+                LoginCodeView(currentStep: $currentStep,
+                         dismissAction: { dismiss() },
+                         viewModel: viewModel)
+           
+            }
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+    
+    func handleBackButton() {
+        switch currentStep {
+        case .phoneNumber:
+            dismiss()
+            viewModel.phoneNumber = ""
+        case .code:
+            currentStep = .phoneNumber
+            viewModel.phoneNumber = ""
+            viewModel.otpText = ""
+
+        }
     }
 }
-
-#Preview {
-    LoginView()
-}
+//
+//#Preview {
+//    LoginView()
+//}
