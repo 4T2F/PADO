7b5f29e5c21057109a9e33ec54569c0151bb78c6
DongHo Choe <hamfan524@naver.com>
2024-01-23 01:08:57 +0900

    🩹 Refactor: 서치바 사용하는 뷰모델들 Searchable 프로토콜 채택

--- PADO/PADO/iOS/Search/SearchViewModel.swift
+++ PADO/PADO/iOS/Search/SearchViewModel.swift
@@ -7,37 +7,11 @@
 
 import SwiftUI
 
-class SearchViewModel: ObservableObject {
+class SearchViewModel: ObservableObject, Searchable {
     @Published var isLoading: Bool = false
     @State var progress: Double = 0
     
     @Published var searchResult: [User] = []
     @Published var viewState: ViewState = ViewState.empty
         
-    public func updateSearchText(with text: String) {
-        setViewState(to: .loading)
-        if text.count > 0 {
-            getSearchResults(forText: text)
-        }
-    }
-    
-    private func getSearchResults(forText text: String) {
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            if self.searchResult.count == 0 {
-                // Empty view
-                self.setViewState(to: .empty)
-            } else {
-                self.setViewState(to: .ready)
-            }
-        }
-    }
-    
-    private func setViewState(to state: ViewState) {
-        DispatchQueue.main.async {
-            self.viewState = state
-            self.isLoading = state == . loading
-        }
-    }
-
 }
