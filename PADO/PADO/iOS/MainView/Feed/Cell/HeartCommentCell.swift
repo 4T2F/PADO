8567ed9b2b98e96f09ced3a898b1a65217061354
kangciu <a2849535@gmail.com>
2024-02-01 23:50:53 +0900

    🐞 Fix: 하트 셀 수정

--- PADO/PADO/iOS/MainView/Feed/Cell/HeartCommentCell.swift
+++ PADO/PADO/iOS/MainView/Feed/Cell/HeartCommentCell.swift
@@ -9,8 +9,6 @@ import Lottie
 import SwiftUI
 
 struct HeartCommentCell: View {
-
-    
     @Binding var isShowingReportView: Bool
     @Binding var isShowingCommentView: Bool
     
@@ -20,74 +18,74 @@ struct HeartCommentCell: View {
     var body: some View {
         VStack(spacing: 16) {
             VStack(spacing: 10) {
-            VStack {
-                if feedVM.selectedFeedCheckHeart {
-                    Button {
-                        Task {
-                            await feedVM.deleteHeart()
-                            feedVM.selectedFeedCheckHeart = await feedVM.checkHeartExists()
+                VStack(spacing: 10) {
+                    if feedVM.selectedFeedCheckHeart {
+                        Button {
+                            Task {
+                                await feedVM.deleteHeart()
+                                feedVM.selectedFeedCheckHeart = await feedVM.checkHeartExists()
+                            }
+                        } label: {
+                            Image("heart_fill")
                         }
-                    } label: {
-                        Image("heart_fill")
-                    }
-                } else {
-                    Button {
-                        Task {
-                            await feedVM.addHeart()
-                            feedVM.selectedFeedCheckHeart = await feedVM.checkHeartExists()
+                    } else {
+                        Button {
+                            Task {
+                                await feedVM.addHeart()
+                                feedVM.selectedFeedCheckHeart = await feedVM.checkHeartExists()
+                            }
+                        } label: {
+                            Image("heart")
                         }
-                    } label: {
-                        Image("heart")
                     }
+                    
+                    // 하트 눌렀을 때 +1 카운팅 되게 하는 로직 추가
+                    Text("\(feedVM.selectedFeedHearts)")
+                        .font(.system(size: 10))
+                        .fontWeight(.semibold)
+                        .shadow(radius: 1, y: 1)
                 }
                 
-                // 하트 눌렀을 때 +1 카운팅 되게 하는 로직 추가
-                Text("\(feedVM.selectedFeedHearts)")
-                    .font(.system(size: 10))
-                    .fontWeight(.semibold)
-                    .shadow(radius: 1, y: 1)
-            }
-            
-            VStack(spacing: 10) {
-                Button {
-                    isShowingCommentView.toggle()
-                } label: {
-                    Image("chat")
+                VStack(spacing: 10) {
+                    Button {
+                        isShowingCommentView.toggle()
+                    } label: {
+                        Image("chat")
+                    }
+                    .sheet(isPresented: $isShowingCommentView) {
+                        CommentView(feedVM: feedVM,
+                                    surfingVM: surfingVM)
+                        .presentationDetents([.fraction(0.99), .fraction(0.8)])
+                        .presentationDragIndicator(.visible)
+                    }
+                    // 댓글이 달릴 때 마다 +1 카운팅 되게 하는 로직 추가
+                    Text(String(feedVM.selectedCommentCounts))
+                        .font(.system(size: 10))
+                        .fontWeight(.semibold)
+                        .shadow(radius: 1, y: 1)
                 }
-                .sheet(isPresented: $isShowingCommentView) {
-                    CommentView(feedVM: feedVM,
-                                surfingVM: surfingVM)
-                    .presentationDetents([.fraction(0.99), .fraction(0.8)])
-                    .presentationDragIndicator(.visible)
-                }
-                // 댓글이 달릴 때 마다 +1 카운팅 되게 하는 로직 추가
-                Text(String(feedVM.selectedCommentCounts))
-                    .font(.system(size: 12))
-                    .fontWeight(.semibold)
-                    .shadow(radius: 1, y: 1)
-            }
-            
-            VStack {
-                Button {
-                    isShowingReportView.toggle()
-                } label: {
-                    VStack {
-                        Text("...")
-                            .font(.system(size: 32))
-                            .fontWeight(.regular)
-                            .foregroundStyle(.white)
-                        
-                        Text("")
+                
+                VStack {
+                    Button {
+                        isShowingReportView.toggle()
+                    } label: {
+                        VStack {
+                            Text("...")
+                                .font(.system(size: 32))
+                                .fontWeight(.regular)
+                                .foregroundStyle(.white)
+                            
+                            Text("")
+                        }
+                    }
+                    .sheet(isPresented: $isShowingReportView) {
+                        ReportSelectView(isShowingReportView: $isShowingReportView)
+                            .presentationDetents([.medium, .fraction(0.8)]) // 모달높이 조절
+                            .presentationDragIndicator(.visible)
                     }
                 }
-                .sheet(isPresented: $isShowingReportView) {
-                    ReportSelectView(isShowingReportView: $isShowingReportView)
-                        .presentationDetents([.medium, .fraction(0.8)]) // 모달높이 조절
-                        .presentationDragIndicator(.visible)
-                }
+                .padding(.top, -15)
             }
-            .padding(.top, -15)
         }
     }
 }
-
