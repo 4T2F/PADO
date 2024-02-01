b44d56a5f5dcecbe505d5c46962cbeed07952c26
DongHo Choe <hamfan524@naver.com>
2024-02-02 06:19:20 +0900

    🌊 Feat: 댓글 누르면 프로필로 이동하도록 구현
    트러블 슈팅
    - 전체화면으로 안 덮이는 문제 발생
    
    Co-Authored-By: ciu <112779139+ha-nabi@users.noreply.github.com>

--- PADO/PADO/iOS/MainView/Comment/Cell/CommentCell.swift
+++ PADO/PADO/iOS/MainView/Comment/Cell/CommentCell.swift
@@ -10,39 +10,59 @@ import SwiftUI
 
 struct CommentCell: View {
     let comment: Comment
+    @State private var user: User? = nil
+    @State private var isUserLoaded = false
     
+    @ObservedObject var feedVM: FeedViewModel
     var body: some View {
         HStack(alignment: .top) {
-
-            if let imageUrl = comment.profileImageUrl {
-                KFImage(URL(string: imageUrl))
-                    .fade(duration: 0.5)
-                    .placeholder{
-                        ProgressView()
-                    }
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 34, height: 34)
-                    .clipShape(Circle())
-            } else {
-                Image("defaultProfile")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 34, height: 34)
-                .padding(.trailing, 6)
+            
+            Button {
+                Task {
+                    // 비동기 함수를 사용하여 사용자 데이터를 가져옵니다.
+                    let fetchedUser = await feedVM.fetchUserData(id: comment.userID)
+                    self.user = fetchedUser
+                    self.isUserLoaded = true // 사용자가 로드되었음을 나타냅니다.
+                }
+            } label: {
+                if let imageUrl = comment.profileImageUrl {
+                    KFImage(URL(string: imageUrl))
+                        .fade(duration: 0.5)
+                        .placeholder{
+                            ProgressView()
+                        }
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 34, height: 34)
+                        .clipShape(Circle())
+                } else {
+                    Image("defaultProfile")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 34, height: 34)
+                        .padding(.trailing, 6)
+                }
             }
-
             
             VStack(alignment: .leading, spacing: 2) {
                 HStack(spacing: 2) {
-                    Text(comment.userID)
-                        .fontWeight(.semibold)
-                        .font(.caption)
-                        .padding(.trailing, 4)
+                    Button {
+                        Task {
+                            // 비동기 함수를 사용하여 사용자 데이터를 가져옵니다.
+                            let fetchedUser = await feedVM.fetchUserData(id: comment.userID)
+                            self.user = fetchedUser
+                            self.isUserLoaded = true // 사용자가 로드되었음을 나타냅니다.
+                        }
+                    } label: {
+                        Text(comment.userID)
+                            .fontWeight(.semibold)
+                            .font(.caption)
+                            .padding(.trailing, 4)
+                    }
+                    
                     Text(TimestampDateFormatter.formatDate(comment.time))
                         .font(.caption)
                         .foregroundColor(.secondary)
-                    
                     
                     Spacer()
                     
@@ -57,6 +77,12 @@ struct CommentCell: View {
                 Text(comment.content)
                     .font(.system(size: 12))
                     .foregroundStyle(.white)
+            }
+        }
+        .navigationDestination(isPresented: $isUserLoaded) {
+            // 'user'가 nil이 아닌 경우에만 OtherUserProfileView를 렌더링합니다.
+            if let user = user {
+                OtherUserProfileView(user: user)
             }
         }
     }
