363fcee5943777761e9588132564f6bb61ee97ad
Hsungjin <hsungjin142@gmail.com>
2024-02-04 23:52:30 +0900

    💄 Design: 이모지 선택 뷰 디자인
    Co-Authored-By: kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>

--- PADO/PADO/iOS/MainView/Comment/View/SelectEmojiView.swift
+++ PADO/PADO/iOS/MainView/Comment/View/SelectEmojiView.swift
@@ -16,135 +16,148 @@ struct SelectEmojiView: View {
     // MARK: - BODY
     var body: some View {
         VStack {
+            Spacer()
+            
             if feedVM.selectedEmoji == "Basic" {
                 ZStack {
                     Circle()
                         .fill(.black)
-                        .stroke(.white, lineWidth: 1.4)
-                        .frame(width: 70, height: 70)
+                        .stroke(.white, lineWidth: 3.0)
+                        .frame(width: 102, height: 102)
                     
                     feedVM.cropMojiImage
                         .resizable()
-                        .frame(width: 66, height: 66)
-                        .cornerRadius(70)
+                        .frame(width: 100, height: 100)
                 }
             } else if feedVM.selectedEmoji == "👍" {
                 ZStack {
                     Circle()
                         .fill(.black)
-                        .stroke(.green, lineWidth: 1.4)
-                        .frame(width: 70, height: 70)
+                        .stroke(.green, lineWidth: 3.0)
+                        .frame(width: 102, height: 102)
                     
                     feedVM.cropMojiImage
                         .resizable()
-                        .frame(width: 66, height: 66)
-                        .cornerRadius(70)
+                        .frame(width: 100, height: 100)
                 }
             } else if feedVM.selectedEmoji == "🥰" {
                 ZStack {
                     Circle()
                         .fill(.black)
-                        .stroke(.pink, lineWidth: 1.4)
-                        .frame(width: 70, height: 70)
+                        .stroke(.pink, lineWidth: 3.0)
+                        .frame(width: 102, height: 102)
                     
                     feedVM.cropMojiImage
                         .resizable()
-                        .frame(width: 66, height: 66)
-                        .cornerRadius(70)
+                        .frame(width: 100, height: 100)
                 }
             } else if feedVM.selectedEmoji == "🤣" {
                 ZStack {
                     Circle()
                         .fill(.black)
-                        .stroke(.yellow, lineWidth: 1.4)
-                        .frame(width: 70, height: 70)
+                        .stroke(.yellow, lineWidth: 3.0)
+                        .frame(width: 102, height: 102)
                     
                     feedVM.cropMojiImage
                         .resizable()
-                        .frame(width: 66, height: 66)
-                        .cornerRadius(70)
+                        .frame(width: 100, height: 100)
                 }
             } else if feedVM.selectedEmoji == "😡" {
                 ZStack {
                     Circle()
                         .fill(.black)
-                        .stroke(.orange, lineWidth: 1.4)
-                        .frame(width: 70, height: 70)
+                        .stroke(.orange, lineWidth: 3.0)
+                        .frame(width: 102, height: 102)
                     
                     feedVM.cropMojiImage
                         .resizable()
-                        .frame(width: 66, height: 66)
-                        .cornerRadius(70)
+                        .frame(width: 100, height: 100)
                 }
             } else if feedVM.selectedEmoji == "😢" {
                 ZStack {
                     Circle()
                         .fill(.black)
-                        .stroke(.blue, lineWidth: 1.4)
-                        .frame(width: 70, height: 70)
+                        .stroke(.blue, lineWidth: 3.0)
+                        .frame(width: 102, height: 102)
                     
                     feedVM.cropMojiImage
                         .resizable()
-                        .frame(width: 66, height: 66)
-                        .cornerRadius(70)
+                        .frame(width: 100, height: 100)
                 }
+            } else {
+                feedVM.cropMojiImage
+                    .resizable()
+                    .frame(width: 100, height: 100)
             }
             
+            Spacer()
+            
             Text("FaceMoji의 이모티콘을 선택해주세요")
+                .font(.system(size: 16))
+            
+        }
+        
+        VStack(alignment: .center) {
+            Spacer()
             
             HStack {
                 Button {
                     feedVM.selectedEmoji = "Basic"
                 } label: {
                     Text("Basic")
-                        .background(.grayButton)
+                        .font(.system(size: 30))
                 }
                 
                 Button {
                     feedVM.selectedEmoji = "👍"
                 } label: {
                     Text("👍")
-                        .background(.grayButton)
+                        .font(.system(size: 30))
                 }
                 
                 Button {
                     feedVM.selectedEmoji = "🥰"
                 } label: {
                     Text("🥰")
-                        .background(.grayButton)
+                        .font(.system(size: 30))
                 }
             }
+            .frame(width: UIScreen.main.bounds.width * 0.9, height: 45)
             
             HStack {
                 Button {
                     feedVM.selectedEmoji = "🤣"
                 } label: {
                     Text("🤣")
-                        .background(.grayButton)
+                        .font(.system(size: 30))
                 }
                 
                 Button {
                     feedVM.selectedEmoji = "😡"
                 } label: {
                     Text("😡")
-                        .background(.grayButton)
+                        .font(.system(size: 30))
                 }
                 
                 Button {
                     feedVM.selectedEmoji = "😢"
                 } label: {
                     Text("😢")
-                        .background(.grayButton)
+                        .font(.system(size: 30))
                 }
             }
+            .frame(width: UIScreen.main.bounds.width * 0.9, height: 45)
             
             Button {
                 feedVM.updateEmoji(emoji: feedVM.selectedEmoji)
             } label: {
                 Text("페이스모지 게시")
+                    .font(.system(size: 16))
                     .foregroundStyle(.white)
-                    .frame(width: UIScreen.main.bounds.width * 0.9, height: 45)
             }
+            .frame(width: UIScreen.main.bounds.width * 0.9, height: 45)
+            .background(.blueButton)
+            .cornerRadius(10)
         }
     }
 }
