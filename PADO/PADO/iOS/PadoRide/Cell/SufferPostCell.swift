729346ee2af6a9368d42e974b9ea3859eb4bcd49
Hsungjin <hsungjin142@gmail.com>
2024-02-08 02:28:59 +0900

    💄 Design: ProgressView 추가
    Co-Authored-By: kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>

--- PADO/PADO/iOS/PadoRide/Cell/SufferPostCell.swift
+++ PADO/PADO/iOS/PadoRide/Cell/SufferPostCell.swift
@@ -10,40 +10,46 @@ import SwiftUI
 
 struct SufferPostCell: View {
     @ObservedObject var padorideVM: PadoRideViewModel
-    var surfingID: String
+    
     @State var sufferPost: [Post]?
+    @State private var isLoading = true
+    
+    var surfingID: String
     
     var body: some View {
         HStack {
-            if let posts = sufferPost {
-                ForEach(posts, id: \.self) { post in
-                    Button {
-                        padorideVM.selectedImage = post.imageUrl
-                    } label: {
-                        ZStack {
-                            // Kingfisher를 사용하여 이미지 로딩
-                            KFImage(URL(string: post.imageUrl))
-                                .onSuccess { _ in
-                                    // 이미지 로딩 성공 시 처리
+            if isLoading {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle())
+                    .scaleEffect(1.5)
+                    .padding()
+                    .padding(.leading, UIScreen.main.bounds.width * 0.45)
+            } else {
+                if let posts = sufferPost {
+                    ForEach(posts, id: \.self) { post in
+                        Button {
+                            padorideVM.selectedImage = post.imageUrl
+                        } label: {
+                            ZStack {
+                                KFImage(URL(string: post.imageUrl))
+                                    .fade(duration: 0.5)
+                                    .placeholder {
+                                        Image("defaultProfile")
+                                            .resizable()
+                                            .scaledToFill()
+                                    }
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(width: 100, height: 150)
+                                
+                                // 선택된 이미지일 경우 체크마크 표시
+                                if padorideVM.selectedImage == post.imageUrl {
+                                    Image(systemName: "checkmark.circle.fill")
+                                        .foregroundColor(.green)
+                                        .background(Circle().fill(Color.white))
+                                        .frame(width: 30, height: 30)
+                                        .offset(x: 35, y: -60)
                                 }
-                                .onFailure { _ in
-                                    // 이미지 로딩 실패 시 처리
-                                }
-                                .placeholder {
-                                    // 이미지 로딩 중 ProgressView 표시
-                                    ProgressView()
-                                }
-                                .resizable()
-                                .scaledToFill()
-                                .frame(width: 100, height: 150)
-                            
-                            // 선택된 이미지일 경우 체크마크 표시
-                            if padorideVM.selectedImage == post.imageUrl {
-                                Image(systemName: "checkmark.circle.fill")
-                                    .foregroundColor(.green)
-                                    .background(Circle().fill(Color.white))
-                                    .frame(width: 30, height: 30)
-                                    .offset(x: 35, y: -60)
                             }
                         }
                     }
@@ -51,8 +57,11 @@ struct SufferPostCell: View {
             }
         }
         .onAppear {
+            // 데이터 로딩 시작
+            isLoading = true
             Task {
                 sufferPost = await padorideVM.getPostData.suffingPostData(id: surfingID)
+                isLoading = false
             }
         }
     }
