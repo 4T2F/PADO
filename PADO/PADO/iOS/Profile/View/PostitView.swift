43cc91498cea5370ef29bf1d4a72b85c1c74088b
DongHo Choe <hamfan524@naver.com>
2024-02-09 05:41:45 +0900

    ✨ Add: PostitView, PostitViewModel 파일 생성

---
+++ PADO/PADO/iOS/Profile/View/PostitView.swift
@@ -0,0 +1,18 @@
+//
+//  PostitView.swift
+//  PADO
+//
+//  Created by 최동호 on 2/9/24.
+//
+
+import SwiftUI
+
+struct PostitView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostitView()
+}
---
+++ PADO/PADO/iOS/Profile/ViewModel/PostitViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  PostitViewModel.swift
+//  PADO
+//
+//  Created by 최동호 on 2/9/24.
+//
+
+import Foundation
+
+class PostitViewModel: ObservableObject {
+    
+}
