285081b294e08e77a22556f7ae65a3b1a14543c6
Hminchae <hminchae@gmail.com>
2024-02-23 05:47:00 +0900

    🌊 Feat: fcm 알림 받을때 로직 수정

--- PADO/PADO/AppDelegate.swift
+++ PADO/PADO/AppDelegate.swift
@@ -13,7 +13,6 @@ import SwiftUI
 import UserNotifications // 푸쉬 알림 탭했을 때 특정 페이지로 이동하기 위함
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-    // @EnvironmentObject var viewModel: AuthenticationViewModel
     
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -36,6 +35,27 @@ class AppDelegate: NSObject, UIApplicationDelegate {
             Messaging.messaging().delegate = self // Firebase 메시징 서비스의 대리자(delegate)를 현재의 AppDelegate 객체로 설정
             
             UNUserNotificationCenter.current().delegate = self
+        }
+        
+        if let launchOptions = launchOptions?[.remoteNotification] {
+            if let notification = launchOptions as? [String: AnyObject],
+               let aps = notification["aps"] as? [String: AnyObject] {
+                
+                if let categoryIdentifier = aps["categoryIdentifier"] as? String {
+                    switch categoryIdentifier {
+                    case "profile":
+                        NotificationCenter.default.post(name: Notification.Name("ProfileNotification"), object: aps["User_id"])
+                    case "post":
+                        NotificationCenter.default.post(name: Notification.Name("PostNotification"), object: aps["Post_id"])
+                    case "postit":
+                        NotificationCenter.default.post(name: Notification.Name("PostitNotification"), object: aps["User_id"])
+                    default:
+                        print("Unknown categoryIdentifier")
+                    }
+                } else {
+                    print("categoryIdentifier is not a string")
+                }
+            }
         }
         return true
     }
@@ -79,6 +99,7 @@ extension AppDelegate: MessagingDelegate {
 // 앱이 실행 중일 때 알림이 도착했을 때 호출
 extension AppDelegate : UNUserNotificationCenterDelegate {
     // foreground 상태에서 푸시알림을 받았을 때 호출되는 함수
+    
     func userNotificationCenter(_ center: UNUserNotificationCenter,
                                 willPresent notification: UNNotification,
                                 withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
@@ -92,58 +113,23 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
                                 didReceive response: UNNotificationResponse,
                                 withCompletionHandler completionHandler: @escaping () -> Void) {
         
-        let application = UIApplication.shared
         let userInfo = response.notification.request.content.userInfo
         let categoryIdentifier = response.notification.request.content.categoryIdentifier
         
-        let formatter = DateFormatter()
-        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss" // ISO 8601 형식
-        formatter.timeZone = TimeZone(secondsFromGMT: 0) // UTC로 설정
-        
+        print(response)
+        print("-------------------")
+        print(userInfo)
         switch categoryIdentifier {
         case "profile":
-            let user = User(
-                id: userInfo["User_id"] as? String,
-                username: userInfo["User_username"] as? String ?? "",
-                lowercasedName: userInfo["User_lowercasedName"] as? String ?? "",
-                nameID: userInfo["User_nameID"] as? String ?? "",
-                profileImageUrl: userInfo["User_profileImageUrl"] as? String,
-                backProfileImageUrl: userInfo["User_backProfileImageUrl"] as? String,
-                date: userInfo["User_date"] as? String ?? "",
-                bio: userInfo["User_bio"] as? String,
-                location: userInfo["User_location"] as? String,
-                phoneNumber: userInfo["User_phoneNumber"] as? String ?? "",
-                fcmToken: userInfo["User_fcmToken"] as? String ?? "",
-                alertAccept: userInfo["User_alertAccept"] as? String ?? "",
-                instaAddress: userInfo["User_instaAddress"] as? String ?? "",
-                tiktokAddress: userInfo["User_tiktokAddress"] as? String ?? ""
-            )
-            NotificationCenter.default.post(name: Notification.Name("ProfileNotification"), object: user)
-            
+            NotificationCenter.default.post(name: Notification.Name("ProfileNotification"), object: userInfo["User_id"])
         case "post":
-            if let createTimeString = userInfo["Post_created_Time"] as? String,
-               let createTime = formatter.date(from: createTimeString){
-                let createdTimestamp = Timestamp(date: createTime)
-                
-                let post = Post(
-                    id: userInfo["Post_id"] as? String,
-                    ownerUid: userInfo["Post_ownerUid"] as? String ?? "",
-                    surferUid: userInfo["Post_surferUid"] as? String ?? "",
-                    imageUrl: userInfo["Post_imageUrl"] as? String ?? "",
-                    title: userInfo["Post_title"] as? String ?? "",
-                    heartsCount: Int(userInfo["Post_heartsCount"] as? String ?? "") ?? 0,
-                    commentCount: Int(userInfo["Post_commentCount"] as? String ?? "") ?? 0,
-                    hearts: nil,
-                    comments: nil,
-                    created_Time: createdTimestamp,
-                    modified_Time: nil,
-                    padoExist: userInfo["padoExist"] as? Bool ?? false
-                )
-                NotificationCenter.default.post(name: Notification.Name("PostNotification"), object: post)
-            }
+            NotificationCenter.default.post(name: Notification.Name("PostNotification"), object: userInfo["Post_id"])
+        case "postit":
+            NotificationCenter.default.post(name: Notification.Name("PostitNotification"), object: userInfo["User_id"])
         default:
             print("categoryIdentifier error")
         }
+        
         HapticHelper.shared.impact(style: .medium) // 햅틱알림
         completionHandler()
     }
@@ -151,6 +137,8 @@ extension AppDelegate : UNUserNotificationCenterDelegate {
     // 원격 알림 수신 처리
     func application(_ application: UIApplication,
                      didReceiveRemoteNotification userInfo: [AnyHashable: Any]) async -> UIBackgroundFetchResult {
+        
+        
         return .newData
     }
 }
